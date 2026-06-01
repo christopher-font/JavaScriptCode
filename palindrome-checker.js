fed2a07fe8c2609ffd296dc3e6998d02d119a6a9
Christopher Font <64507939+christopher-font@users.noreply.github.com>
2026-06-01 14:32:41 -0400

    Update palindrome-checker.js
    Removed Whitespace and renamed function "palindrome" to "isPalindrome"

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,18 +1,13 @@
-function palindrome(str) {
+function isPalindrome(str) {
   str = str.toLowerCase();
-
   let regex = /\W|_/g;
-
   str = str.replace(regex, '');
-
   for (var i = 0; i < str.length; i++) {
     var j = str.length - 1 - i;
-
     if (str[i] != str[j]) 
       return false;
   }
-
   return true;
 }
 
-console.log(palindrome("race car"));
+console.log(isPalindrome("race car"));
